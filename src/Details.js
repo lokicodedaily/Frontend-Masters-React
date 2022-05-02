4074f0c84e642971a8428d75bf4fde3e9138ef15
loki <lokidigari@gmail.com>
2022-05-02 22:46:34 +0530

    changed details to class component and present the details

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,40 @@
-const Details = () => {
-  return <h2>Hi! lololo omg wtf</h2>
+import React, { Component } from "react";
+
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2> {`${animal} - ${breed} - ${city} - ${state}`} </h2>
+        <button>Adpot {name}</button>
+        <p>{description}</p>
+      </div>
+    );
+  }
 }
 
-export default Details;
+export default withRouter(Details);
